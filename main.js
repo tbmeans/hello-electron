aecf958f38addbcb8a5613bee2edada94fbe2be6
Timothy Means <tbmeans2005@gmail.com>
2024-02-12 03:29:13 -0500

    dark mode

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, nativeTheme } = require('electron');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600
   });
+
+  nativeTheme.themeSource = 'dark';
 
   win.loadFile('index.html');
 };
